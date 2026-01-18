8b3b0315b4349acbbcadf6b7999763c673e7560a
Eslemnureryigit <251012045@st.atlas.edu.tr>
2026-01-19 00:09:46 +0300

    Add files via upload

---
+++ Döngüler.java
@@ -0,0 +1,54 @@
+import java.util.Iterator;
+public class Döngüler {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*	DÖNGÜLER
+		For
+		While
+		Do-While
+		Foreach
+		
+  FOR : 3 parçadan oluşur 1.yerde başlangıç 2.yerde koşul  3.yerde arrtırma ya da azaltma işlemleri 
+		
+		for (int a = baslangiç ; kosul ; arttırma )
+		
+		
+		1den 10a kadar sayıları yazma
+		*/
+		
+		/*for(int e=1 ; e<=10 ; e++)
+		{
+		System.out.println(e);
+		}
+		
+		1den 10a kadar çift sayıları yazdırma
+		for(int i =1 ; i<=10; i++ )
+		{
+			if (i%2==0);
+			{
+		System.out.println(i);
+		
+		1den 10a kadar tek yazdırma
+		for(int i =1 ; i<=10; i++ )
+		{
+			if (i%2==1);
+			{
+		System.out.println(i);
+		
+		
+		1den 10a kadar çift sayıların toplamı
+		int toplam = 0;
+		for(int i =1 ; i<=10; i++ )
+		if(i%2==0)
+		toplam += i;
+				System.out.println("Sonuc:" + toplam);
+
+			}*/
+		}
+	}
+
+	
+
+
+
